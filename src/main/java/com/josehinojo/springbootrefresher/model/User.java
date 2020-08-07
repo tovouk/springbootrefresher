14d9108e5a80d72c4baff581873b039d5aa7cffa
jose <burningremedy@gmail.com>
2020-08-06 23:03:29 -0500

    implement restful api methods

--- src/main/java/com/josehinojo/springbootrefresher/model/User.java
+++ src/main/java/com/josehinojo/springbootrefresher/model/User.java
@@ -5,11 +5,11 @@ import java.util.UUID;
 public class User {
 	
 	private UUID userUid;
-	private final String firstName;
-	private final String lastName;
-	private final Gender gender;
-	private final int age;
-	private final String email;
+	private String firstName;
+	private String lastName;
+	private Gender gender;
+	private int age;
+	private String email;
 	
 	public enum Gender{
 		MALE,
@@ -39,10 +39,47 @@ public class User {
 	public String getEmail() {
 		return email;
 	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setUserUid(UUID userUid2) {
+		this.userUid = userUid2;
+	}
+	
+	public User() {
+		
+	}
 
 	public User(UUID userUid, String firstName, String lastName, Gender gender, int age, String email) {
 		super();
 		this.userUid = userUid;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.age = age;
+		this.email = email;
+	}
+	
+	public User(String firstName, String lastName, Gender gender, int age, String email) {
+		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.gender = gender;
@@ -55,10 +92,6 @@ public class User {
 	public String toString() {
 		return "User [userUid=" + userUid + ", firstName=" + firstName + ", lastName=" + lastName + ", gender=" + gender
 				+ ", age=" + age + ", email=" + email + "]";
-	}
-
-	public void setUserUid(UUID userUid2) {
-		this.userUid = userUid2;
 	}
 	
 }
--- src/main/java/com/josehinojo/springbootrefresher/resource/UserResource.java
+++ src/main/java/com/josehinojo/springbootrefresher/resource/UserResource.java
@@ -1,8 +1,15 @@
 package com.josehinojo.springbootrefresher.resource;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +28,63 @@ public class UserResource {
 		this.userService = userService;
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, path = "hello")
+	@RequestMapping(method = RequestMethod.GET)
 	public List<User> fetchUsers(){
 		return userService.getAllUsers();
 	}
 	
+	@RequestMapping(
+		method = RequestMethod.GET,
+		path = "{userUid}"
+	)
+	public ResponseEntity<?> fetchUser(@PathVariable("userUid") UUID userUid) {
+		return userService.getUser(userUid).<ResponseEntity<?>>map(ResponseEntity::ok)
+				.orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
+						.body(new ErrorMessage("user " + userUid + " was not found.")));
+	}
+	
+	//Only Accepts JSON values from client
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Integer> insertNewUser(@RequestBody User user) {
+		int result = userService.insertUser(user);
+		return getIntegerResponse(result);
+	}
+	
+	@RequestMapping(method =  RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Integer> updateUser(@RequestBody User user){
+		int result = userService.updateUser(user);
+		return getIntegerResponse(result);
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE,
+			path = "{userUid}")
+	public ResponseEntity<Integer> deleteUser(@PathVariable("userUid") UUID userUid){
+		int result = userService.removeUser(userUid);
+		return getIntegerResponse(result);
+	}
+	
+	private ResponseEntity<Integer> getIntegerResponse(int result) {
+		if(result == 1) {
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.badRequest().build();
+	}
+	
+	class ErrorMessage {
+		String errorMessage;
+
+		public ErrorMessage(String errorMessage) {
+			super();
+			this.errorMessage = errorMessage;
+		}
+
+		public String getErrorMessage() {
+			return errorMessage;
+		}
+
+		public void setErrorMessage(String errorMessage) {
+			this.errorMessage = errorMessage;
+		}
+		
+	}
 }
