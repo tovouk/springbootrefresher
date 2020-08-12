2f289e4b1e86b7563a7b9c85e75b2ad864de015f
Jose Hinojo <burningremedy@gmail.com>
2020-08-12 17:25:17 -0500

    Application properties variable implementation

--- src/main/java/com/josehinojo/springbootrefresher/clientproxy/ClientProxyConfig.java
+++ src/main/java/com/josehinojo/springbootrefresher/clientproxy/ClientProxyConfig.java
@@ -4,19 +4,24 @@ import javax.ws.rs.client.ClientBuilder;
 
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ClientProxyConfig {
 	
+	@Value("${users.api.url.v1}")
+	private String usersEndpointUrl;
+	
 	@Bean
 	public UserResourceV1 getUserResourceV1() {
 		ResteasyClient client = (ResteasyClient)ClientBuilder.newClient();
-		final String usersEndpointUrl = "http://localhost:8080/api/v1/users";
 		ResteasyWebTarget target = client.target(usersEndpointUrl);
 		UserResourceV1 proxy = target.proxy(UserResourceV1.class);
 		return proxy;
 	}
+	
+	
 	
 }
